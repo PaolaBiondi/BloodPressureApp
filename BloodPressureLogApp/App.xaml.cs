534d016a919cd610c5ddd9a9d02e33b680bba52e
homedevops <homedevops@outlook>
2024-02-09 16:01:59 +0100

    Add project files.

---
+++ BloodPressureLogApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BloodPressureLogApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
