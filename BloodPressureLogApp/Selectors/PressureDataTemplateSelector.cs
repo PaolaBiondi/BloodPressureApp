302e6abddbbf2ba367a380c4278ee9ecfc6e62d1
Paola <ml3oepl0@duck.com>
2024-03-20 13:11:05 +0100

    Adding CollectionView DataTemplate Style

---
+++ BloodPressureLogApp/Selectors/PressureDataTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodPressureLogApp.Selectors
+{
+    public class PressureDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var pressure = item as Pressure;
+
+            if (pressure?.Systolic > 120)
+            {
+                Application.Current!.Resources.TryGetValue("PressureItemStyle", out var dtPressure);
+                return dtPressure as DataTemplate ?? new DataTemplate();
+            }
+
+            Application.Current!.Resources.TryGetValue("WarningPressureItemStyle", out var dtWarning);
+            return dtWarning as DataTemplate ?? new DataTemplate();
+        }
+    }
+}
